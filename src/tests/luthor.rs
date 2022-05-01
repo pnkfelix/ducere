343925a8ef44547d6e91f0430a47b4b39c37ffda
Felix S. Klock II <pnkfelix@pnkfx.org>
2022-04-30 23:13:10 -0400

    revised tests.
    factored out some sub-categories of the basics tests.
    
    made a macro for comparing iterations in a manner that we can see the result
    when the test fails. (by collecting them into vectors).

--- src/tests/luthor.rs
+++ src/tests/luthor.rs
@@ -12,34 +12,52 @@ fn strings<'a>(v: &'a [&str]) -> impl Iterator<Item=String> + 'a {
     v.iter().map(|s|s.to_string())
 }
 
+macro_rules! assert_strings_eq {
+    ($lft:expr, $rgt:expr) => {
+        // assert!($lft.eq($rgt))
+        assert_eq!($lft.collect::<Vec<String>>(), $rgt.collect::<Vec<String>>())
+    }
+}
+
 #[test]
 fn lexing_basics() {
     assert!(lex("").eq(strings(&[]).map(|s|Ok(s))));
-    assert!(lex_oks("a").eq(strings(&["a"])));
-    assert!(lex_oks("ab").eq(strings(&["ab"])));
-    assert!(lex_oks("a b").eq(strings(&["a", " ", "b"])));
-    assert!(lex_oks("(a b)").eq(strings(&["(", "a", " ", "b", ")"])));
-    assert!(lex_oks("(a b").eq(strings(&["(", "a", " ", "b"])));
-    assert!(lex_oks("((a b").eq(strings(&["(", "(", "a", " ", "b"])));
-    assert!(lex_oks("([{a b").eq(strings(&["(", "[", "{", "a", " ", "b"])));
-    assert!(lex_oks(r##"a b"##).eq(strings(&["a", " ", "b"])));
-    assert!(lex_oks(r##"a b c"##).eq(strings(&["a", " ", "b", " ", "c"])));
-    assert!(lex_oks(r##"a r#"b"# c"##).eq(strings(&["a", " ", "b", " ", "c"])));
-    assert!(lex_oks(r##"a r#"b b"# c"##).eq(strings(&["a", " ", "b b", " ", "c"])));
-    assert!(lex_oks(r##"a r#[b b]# c"##).eq(strings(&["a", " ", "b b", " ", "c"])));
+    assert_strings_eq!(lex_oks("a"), strings(&["a"]));
+    assert_strings_eq!(lex_oks("ab"), strings(&["ab"]));
+    assert_strings_eq!(lex_oks("a b"), strings(&["a", " ", "b"]));
+    // this is more a meta-test: Its just pointing out that we'll be using raw-strings
+    // in a lot of these tests, and that its important to not confuse the raw-strings
+    // that *surround* the test input (which tend to have two or more #'s) with raw-strings
+    // that are *in* the test input (which tend to have just zero or one #'s, though I did
+    // use two #'s for cases where its matching non-quote forms internally.).
+    assert_strings_eq!(lex_oks(r##"a b"##), strings(&["a", " ", "b"]));
+    assert_strings_eq!(lex_oks(r##"a b c"##), strings(&["a", " ", "b", " ", "c"]));
+}
+#[test]
+fn lexing_brackets() {
+    assert_strings_eq!(lex_oks("(a b)"), strings(&["(", "a", " ", "b", ")"]));
+    assert_strings_eq!(lex_oks("(a b"), strings(&["(", "a", " ", "b"]));
+    assert_strings_eq!(lex_oks("((a b"), strings(&["(", "(", "a", " ", "b"]));
+    assert_strings_eq!(lex_oks("([{a b"), strings(&["(", "[", "{", "a", " ", "b"]));
+}
+#[test]
+fn lexing_quotations() {
+    assert_strings_eq!(lex_oks(r##"a r#"b"# c"##), strings(&["a", " ", "b", " ", "c"]));
+    assert_strings_eq!(lex_oks(r##"a r#"b b"# c"##), strings(&["a", " ", "b b", " ", "c"]));
+    assert_strings_eq!(lex_oks(r##"a r#[b b]# c"##), strings(&["a", " ", "b b", " ", "c"]));
 
-    assert!(lex_oks(r##"a r##[b [#] b]## c"##)
-            .eq(strings(&["a", " ", "b [#] b", " ", "c"])));
+    assert_strings_eq!(lex_oks(r##"a r##[b [#] b]## c"##),
+                       strings(&["a", " ", "b [#] b", " ", "c"]));
 
-    assert!(lex_oks(r##"a r##[b #### [#] #### b]## c"##)
-            .eq(strings(&["a", " ", "b #### [#] #### b", " ", "c"])));
+    assert_strings_eq!(lex_oks(r##"a r##[b #### [#] #### b]## c"##),
+                       strings(&["a", " ", "b #### [#] #### b", " ", "c"]));
 
-    assert!(lex_oks(r##"a r#[A]#r#(B)#r#{C}#r#"D"#r#'E'#r#<F>#r#|G|#r#`H`# c"##)
-            .eq(strings(&["a", " ", "A", "B", "C", "D", "E", "F", "G", "H", " ", "c"])));
+    assert_strings_eq!(lex_oks(r##"a r#[A]#r#(B)#r#{C}#r#"D"#r#'E'#r#<F>#r#|G|#r#`H`# c"##),
+                       strings(&["a", " ", "A", "B", "C", "D", "E", "F", "G", "H", " ", "c"]));
 
-    assert!(lex_oks(&[r##"a r[A(B)]r(C[D])r{[E](F)}r"{G}'H'""##,
-                      r##"r'I"J"'r#<KL>#r#|MN|#r#`OP`# c"##]
-                    .into_iter().collect::<String>())
-            .eq(strings(&["a", " ", "A(B)", "C[D]", "[E](F)", "{G}'H'",
-                          "I\"J\"", "KL", "MN", "OP", " ", "c"])));
+    assert_strings_eq!(lex_oks(&[r##"a r[A(B)]r(C[D])r{[E](F)}r"{G}'H'""##,
+                                 r##"r'I"J"'r#<KL>#r#|MN|#r#`OP`# c"##]
+                               .into_iter().collect::<String>()),
+                       strings(&["a", " ", "A(B)", "C[D]", "[E](F)", "{G}'H'",
+                                 "I\"J\"", "KL", "MN", "OP", " ", "c"]));
 }
