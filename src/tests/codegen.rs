ea0eacb1b61543f22438c565a4fc9a0a84d1f1cf
Felix S. Klock II <pnkfelix@pnkfx.org>
2023-10-21 15:14:03 -0400

    Make java variant of the rust demo codegen test.

--- src/tests/codegen.rs
+++ src/tests/codegen.rs
@@ -9,19 +9,20 @@ enum CodegenTestError {
     Io(#[from] std::io::Error),
     #[error("codegen utf8 conversion error: {0}")]
     Utf8(#[from] std::string::FromUtf8Error),
-    #[error("codegen compile fail: {0}")]
+    #[error("codegen compile fail: {0:?}")]
     CompileFail(FailedCommand),
-    #[error("codegen invoke fail: {0}")]
+    #[error("codegen invoke fail: {0:?}")]
     InvokeFail(FailedCommand),
 }
 
-#[derive(Debug)]
 struct FailedCommand { stdout: Vec<u8>, stderr: Vec<u8> }
 
-impl std::fmt::Display for FailedCommand {
+impl std::fmt::Debug for FailedCommand {
     fn fmt(&self, w: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(w, "stdout: {}", String::from_utf8_lossy(&self.stdout))?;
-        write!(w, "stderr: {}", String::from_utf8_lossy(&self.stderr))
+        write!(w, r##"FailedCommand {{\n"##)?;
+        write!(w, r##"    stdout: r#"{}"#\n"##, String::from_utf8_lossy(&self.stdout))?;
+        write!(w, r##"    stderr: r#"{}"#\n"##, String::from_utf8_lossy(&self.stderr))?;
+        write!(w, "}}")
     }
 }
 
@@ -41,7 +42,7 @@ impl CodegenTestError {
 }
 
 #[test]
-fn baseline() -> Result<(), CodegenTestError> {
+fn baseline_echo_in_rust() -> Result<(), CodegenTestError> {
     let input = r#"Hello World!"#;
     let code = r#"
 fn main() -> Result<(), std::io::Error> {
@@ -70,10 +71,59 @@ fn main() -> Result<(), std::io::Error> {
     let invoke_output = std::process::Command::new(output_path)
         .stdin(std::fs::File::open(input_temp.path())?)
         .output()?;
-    assert!(invoke_output.status.success());
     if !invoke_output.status.success() {
-        return Err(CodegenTestError::invoke_fail(invoke_output.clone()));
+        let err = CodegenTestError::invoke_fail(invoke_output.clone());
+        println!("err: {err}");
+        return Err(err);
     }
+    assert!(invoke_output.status.success());
+    expect!("Hello World!\n").assert_eq(&String::from_utf8(invoke_output.stdout.clone())?);
+    Ok(())
+}
+
+#[test]
+fn baseline_echo_in_java() -> Result<(), CodegenTestError> {
+    let input = r#"Hello World!"#;
+    let code = r#"
+import java.util.Scanner;
+class Echo {
+    public static void main(String[] args) {
+        String inData;
+        Scanner scan = new Scanner( System.in );
+
+        while (scan.hasNextLine()) {
+            inData = scan.nextLine();
+            System.out.println(inData);
+        }
+    }
+}
+"#;
+    let source_temp = temp_file::TempFile::with_suffix(".java")?
+        .with_contents(code.as_bytes())?;
+    let input_temp = temp_file::with_contents(input.as_bytes());
+    let output_path = temp_dir::TempDir::new()?;
+    let compile_output = std::process::Command::new("javac")
+        .arg("-d")
+        .arg(output_path.path())
+        .arg(source_temp.path())
+        .output()?;
+    if !compile_output.status.success() {
+        let err = CodegenTestError::compile_fail(compile_output);
+        println!("err: {err}");
+        return Err(err);
+    }
+    let invoke_output = std::process::Command::new("java")
+        .arg("-cp")
+        .arg(output_path.path())
+        .arg("Echo")
+        .stdin(std::fs::File::open(input_temp.path())?)
+        .output()?;
+    if !invoke_output.status.success() {
+        let err = CodegenTestError::invoke_fail(invoke_output.clone());
+        println!("err: {err}");
+        return Err(err);
+   }
+    assert!(invoke_output.status.success());
     expect!("Hello World!\n").assert_eq(&String::from_utf8(invoke_output.stdout.clone())?);
     Ok(())
 }
